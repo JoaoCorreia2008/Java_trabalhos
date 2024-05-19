414b0dff1bfb5c008a62453f9eb94dfd63fcaffc
uabo6969 <147398180+uabo6969@users.noreply.github.com>
2024-05-19 19:29:29 +0100

    Add files via upload

--- Pessoa/ Pessoa/src/App.java
+++ Pessoa/ Pessoa/src/App.java
@@ -1,11 +1,13 @@
+import java.time.LocalDate;
 public class App {
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello, World!");
 
         Pessoa pessoa1 =  new Pessoa("Joao Correia",2008,2,9,null,"Eva");
         Pessoa  Pessoa =  new Pessoa("Joao Correia",2008,2,9,null,"Eva");
-        System.out.println();
+        System.out.println(pessoa1);
         System.out.println(Pessoa);
+        System.out.println(LocalDate.now());
         
     }
 }
